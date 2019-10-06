5991d59fdbaca0e66b42c0b5ce3ba3a949daf15e
beanbroker <88p7h21@gmail.com>
2019-10-06 15:54:40 +0900

    init

---
+++ src/test/kotlin/com/beanbroker/broker/BrokerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.beanbroker.broker
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BrokerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
